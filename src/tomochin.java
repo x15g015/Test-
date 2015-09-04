e4108e6c1fec6acbc30b3e1d02a352b8024519ae
unknown <x15g015@PC505N003.chiba-fjb.ac.jp>
2015-09-04 12:13:29 +0900

    a

--- src/tomochin.java
+++ src/tomochin.java
@@ -3,7 +3,9 @@ public class tomochin {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		System.out.println("前田敦子");
+		System.out.println("柏木由紀"); 
+		System.out.println("板野友美");
 	}
 
 }
